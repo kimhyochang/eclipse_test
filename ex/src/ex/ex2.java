a0be9bf25e9427c616b4abf2a28efeec8a820f09
DESKTOP-KIJTI1U\elina <elinan@naver.com>
2017-04-27 00:07:49 +0900

    브랜치에 'nice to meet you' 추가

--- ex/src/ex/ex2.java
+++ ex/src/ex/ex2.java
@@ -5,6 +5,7 @@ public class ex2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
+		System.out.println("Nice to meet you!");
 	}
 
 }
