36b1cd47b758d5ad3d77c31b94e7ef24977a235f
DESKTOP-KIJTI1U\elina <elinan@naver.com>
2017-04-27 01:03:11 +0900

    '자기소개' 추가

--- ex/src/ex/ex2.java
+++ ex/src/ex/ex2.java
@@ -6,6 +6,7 @@ public class ex2 {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
 		System.out.println("Nice to meet you!");
+		System.out.println("My name is Kim");
 	}
 
 }
